b96dfed8c766bf0ef70bda0608cf89c9b0cd8803
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2026-01-01 22:47:49 -0600

    Update DVDDemuxFFmpeg.cpp

--- xbmc/cores/VideoPlayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
+++ xbmc/cores/VideoPlayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
@@ -421,29 +421,36 @@ bool CDVDDemuxFFmpeg::Open(const std::shared_ptr<CDVDInputStream>& pInput, bool 
           // AC3 is always wrapped in iec61937 (ffmpeg "spdif"), while DTS
           // may be just padded.
           const AVInputFormat* iformat2 = av_find_input_format("spdif");
-          if (iformat2 && iformat2->read_probe(&pd) > AVPROBE_SCORE_MAX / 4)
+          if (iformat2)
           {
-            iformat = iformat2;
-          }
-          else
-          {
-            // not spdif or no spdif demuxer, try dts
-            iformat2 = av_find_input_format("dts");
-
-            if (iformat2 && iformat2->read_probe(&pd) > AVPROBE_SCORE_MAX / 4)
+            int score = 0;
+            const AVInputFormat* detected = av_probe_input_format2(&pd, 1, &score);
+            if (detected == iformat2)
             {
               iformat = iformat2;
             }
-            else if (trySPDIFonly)
+            else
             {
-              // not dts either, return false in case we were explicitly
-              // requested to only check for S/PDIF padded compressed audio
-              CLog::Log(LOGDEBUG, "{} - not spdif or dts file, falling back", __FUNCTION__);
-              return false;
+              // not spdif or no spdif demuxer, try dts
+              iformat2 = av_find_input_format("dts");
+              if (iformat2)
+              {
+                score = 0;
+                detected = av_probe_input_format2(&pd, 1, &score);
+                if (detected == iformat2)
+                {
+                  iformat = iformat2;
+                }
+                else if (trySPDIFonly)
+                {
+                  // not dts either, return false in case we were explicitly
+                  // requested to only check for S/PDIF padded compressed audio
+                  CLog::Log(LOGDEBUG, "{} - not spdif or dts file, falling back", __FUNCTION__);
+                  return false;
+                }
+              }
             }
           }
-        }
-      }
 
       if (!iformat)
       {
@@ -1353,7 +1360,11 @@ bool CDVDDemuxFFmpeg::SeekTime(double time, bool backwards, double* startpts)
 
     if (ret >= 0)
     {
+    #if LIBAVFORMAT_VERSION_MAJOR < 59
       if (m_pFormatContext->iformat->read_seek)
+    #else
+      if (m_pFormatContext->iformat->flags & AVFMT_SEEK_TO_PTS)
+    #endif
         m_seekToKeyFrame = true;
       m_currentPts = DVD_NOPTS_VALUE;
     }
